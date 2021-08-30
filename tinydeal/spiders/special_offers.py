ffdd00f1e55e42f72662c519b45276817352515d
Salim Mzoughi <salim.mzoughi@ecole-it.com>
2021-08-30 17:43:02 +0200

    Initial commit

---
+++ tinydeal/spiders/special_offers.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class SpecialOffersSpider(scrapy.Spider):
+    name = 'special_offers'
+    allowed_domains = ['web.archive.org']
+    start_urls = ['https://web.archive.org/web/20190225123327/https://www.tinydeal.com/specials.html/']
+
+    def parse(self, response):
+        pass
